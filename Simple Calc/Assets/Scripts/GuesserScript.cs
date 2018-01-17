14099d6748812c0f0fc1a59dd37614fe5191570a
Borkl <broklbrookes@gmail.com>
2018-01-17 09:52:57 -0700

    Adding more onto script

--- Simple Calc/Assets/Scripts/GuesserScript.cs
+++ Simple Calc/Assets/Scripts/GuesserScript.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class GuesserScript : MonoBehaviour {
 
+    public int max;
+    public int min;
+    public int guess;
+
+
 	// Use this for initialization
 	void Start () {
-		print ("Our game has started!");
+		print ("Welcome to Number Guesser");
+        print("Pick a number between" + min + "and" + max);
+
+        //Is the value GUESS
+        print("Is the number" + guess + "?");
+        //Instruction - Push these buttons
+        print("Up arrow for higher, Down arrow for lower, enter for correct");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            guess = (min + max) / 2;
+            print("Is the number" + guess + "?");
+        }
+
+        //Up arrow
+
+        //Enter button
 	}
 }
