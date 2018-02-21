4167b23442b552d3be3602eb9d08c986b93bf563
Borkl <broklbrookes@gmail.com>
2018-02-21 09:58:07 -0700

    brick count and text adevnture?

---
+++ Brick count/Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public static int brickCount;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void LoadLevel(string level)
+    {
+        brickCount = 0;
+        SceneManager.LoadScene(level);
+    }
+
+    public void LoadNextLevel()
+    {
+        brickCount = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+}
